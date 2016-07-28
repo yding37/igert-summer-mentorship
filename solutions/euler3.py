66cb2676910f19b0d30e5185039da9c4c102cffb
Yi Ding <yding37@gmail.com>
2016-07-28 16:11:20 -0700

    added solutions for euler

---
+++ solutions/euler3.py
@@ -0,0 +1,16 @@
+
+def get_primes(max):
+    """
+    Returns list of prime numbers up to given max
+    """
+
+    # TODO: What to set max to?
+    # TODO: Compute list of prime numbers
+
+    return None
+
+
+if __name__=="__main__":
+    # TODO: get list of prime numbers
+    # TODO: find largest prime factor of 600851475143
+    
